6d35090f94a6c59bfaaa7daf4c0da2622fb9e311
AlexandreH13 <alexandreh133@gmail.com>
2016-10-27 09:39:37 -0200

    Commite de teste

---
+++ app/src/main/java/com/example/italo/adoteumpet/ui/PessoaCadastroEditarActivity.java
@@ -0,0 +1,40 @@
+package com.example.italo.adoteumpet.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.italo.adoteumpet.R;
+
+//Comentário inútil
+public class PessoaCadastroEditarActivity extends AppCompatActivity {
+
+    private EditText nomePessoa;
+    private  EditText endereco;
+    private EditText numero;
+    private EditText bairro;
+    private EditText estado;
+    private EditText cidade;
+    private Button confirmar;
+    private  Button cancelar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pessoa_cadastrar_editar);
+
+        nomePessoa = (EditText) findViewById(R.id.pessoa_cadastrarEditar);
+        endereco = (EditText) findViewById(R.id.cad_pessoa_endereco);
+        numero = (EditText) findViewById(R.id.cad_pessoa_endereco_numero);
+        bairro = (EditText) findViewById(R.id.cad_pessoa_bairro);
+        estado = (EditText) findViewById(R.id.cad_pessoa_estado);
+        cidade = (EditText) findViewById(R.id.cad_pessoa_cidade);
+
+        confirmar = (Button) findViewById(R.id.btnCadastrar);
+        cancelar = (Button) findViewById(R.id.btnCancelar);
+    }
+
+    public
+
+}
